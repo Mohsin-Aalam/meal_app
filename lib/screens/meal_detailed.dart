d9796cb393f452fedc2b94efaa1a6efa67534485
mohsin alam <greenstore987@gmail.com>
2023-09-09 14:01:11 +0530

    first

---
+++ lib/screens/meal_detailed.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/provider/favorite_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MealDetailsScreen extends ConsumerWidget {
+  const MealDetailsScreen({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsprovider);
+    final isFavorite = favoriteMeals.contains(meal);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              final wasAdded = ref
+                  .read(favoriteMealsprovider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(wasAdded
+                      ? 'Meal added as a favorite.'
+                      : 'Meal removed .'),
+                ),
+              );
+            },
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              'indredient',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final index in meal.ingredients)
+              Text(
+                index,
+                style:
+                    const TextStyle(color: Color.fromARGB(125, 252, 249, 249)),
+              ),
+            const SizedBox(
+              height: 14,
+            ),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            for (final index in meal.steps)
+              Text(
+                index,
+                style: const TextStyle(
+                    color: Color.fromARGB(125, 252, 249, 249), height: 2),
+                softWrap: true,
+                textAlign: TextAlign.center,
+              ),
+            const SizedBox(
+              height: 14,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
